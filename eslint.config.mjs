b72d25afa578abeeea7039c7d8febe278e06ca99
Deepak-Kumar123-creator <dc3648237@gmail.com>
2026-09-21 18:18:01 +0530

    Fix ESLint configuration and JSX apostrophes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,17 @@
 ﻿import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default defineConfig([
+  ...compat.extends("next/core-web-vitals"),
   globalIgnores([
     ".next/**",
     "out/**",
@@ -10,5 +19,3 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
