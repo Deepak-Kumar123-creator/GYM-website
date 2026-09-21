a4f1ec15e92e8928791b120cc9806d4468b80553
Deepak-Kumar123-creator <dc3648237@gmail.com>
2026-09-21 18:04:07 +0530

    Fix Next.js security update and Vercel ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
-import {defineConfig,globalIgnores} from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
-export default defineConfig([...nextVitals,globalIgnores(['.next/**','node_modules/**'])]);
+﻿import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
